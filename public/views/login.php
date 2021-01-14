8bc794bfdc2d5896f6650f0b7373619a003c82b9
bartq98 <bartusiekkrosno@poczta.fm>
2021-01-14 02:39:58 +0100

    Add styles and two basic views
    Add temporary logo.png (will be changed to .svg version)
    Add normalize.css and style.css and make them working
    Add two basic, currently empty views workout.php and nutrition.php

--- public/views/login.php
+++ public/views/login.php
@@ -1,7 +1,25 @@
 <!doctype html>
 <html>
+
 <head>
+    <meta charset="utf-8">
+    <title>fitU - panel logowania</title>
+
+    <link rel="stylesheet" href="/public/style/normalize.css">
+    <link rel="stylesheet" href="/public/style/style.css">
 </head>
-<body>Too lazy to write something clever</body>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="/public/img/logo.png">
+        </div>
+        <form>
+            <input name="email" type="text" placeholder="your@email.com">
+            <input name="password" type="password" placeholder="your@email.com">
+            <button>Login</button>
+        </form>
+    </div>
+</body>
 
 </html>
+
