f29d93deb3d33ec79f68bec7a090de546b4964b3
bartq98 <bartusiekkrosno@poczta.fm>
2021-01-18 03:12:17 +0100

    Configure database
    Add database class to get connection with Heroku-hosted DB.
    
    SecurityController now works properly:
      - get user by it's e-mail
      - login or get proper message within login panel
    There was also small bug (method check should be `!$this->isPost()`.
    
    UserRepository are wrapper for creating user from DB. It:
      - connets to DB
      - gets proper user if exists
      - return null or return User with correct data
    
    UserRepository getUser method should throws exceptions rather than
    return null # TODO
    
    Dockerfile for PHP has changed to work with PDO library

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,21 +2,35 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
     public function login() {
-        $user = new User("john@snow.com", "12345", "John", "Snow");
 
-        if ($this->isGet()) {
+        $userRepository = new UserRepository();
+
+
+        if (!$this->isPost()) {
             return $this->login('login');
         }
 
         $email = $_POST["email"];
         $password = $_POST["password"];
 
+        $user = $userRepository->getUser($email);
+
+        if (!$user) {// if there is not user with provided email in database
+            // get login panel with proper message
+            return $this->render('login', ['messages' => ["User with provided email not exists."]]);
+        }
+
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ["User with provided email not exists."]]);
+        }
+
+        if ($user->getPassword() !== $password) {
+            return $this->render('login', ['messages' => ["Invalid password."]]);
         }
 
         $url = "http://$_SERVER[HTTP_HOST]";
