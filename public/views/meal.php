a1bb2cfd2c94de31356560835e43bae4063bc1bc
bartq98 <bartusiekkrosno@poczta.fm>
2021-01-21 04:08:55 +0100

    Add proper colors (shades of blue)

--- public/views/meal.php
+++ public/views/meal.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="headerbutton">
                     <i class="fas fa-user-alt"></i>
-                    Dane osob
+                    Dane osobowe
                 </div>
             </div>
         </header>
@@ -55,7 +55,7 @@
 
         </nav>
         <main>
-            <div>Właściwy kontent tutaj</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
         </main>
     </div>
 </body>
